55d74ef309275c6ecad273653082615a068ea430
yuuki <yuki01377@gmail.com>
2020-01-08 08:41:29 +0900

    ルーティング

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,22 @@ import { HeroesComponent } from './heroes/heroes.component';
 import { FormsModule } from '@angular/forms';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
+import { AppRoutingModule } from './app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
 // 全てのコンポーネントはただ一つのNgModuleで宣言しないといけない
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
     HeroDetailComponent,
-    MessagesComponent
+    MessagesComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,6 @@
 // @angular/coreライブラリからcomponentシンボルをインポート
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-// import { HEROES } from '../mock-heroes';
 import { HeroService } from '../hero.service';
 @Component({
   selector: 'app-heroes',
@@ -9,15 +8,11 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  // hero = 'windstorem';
   hero: Hero = {
     id: 1,
     name: 'windstorm'
   };
-  // 本来はデータの取得と保存はしてはいけない
-  // heroes = HEROES;
   heroes: Hero[];
-  selectedHero: Hero;
   constructor(
     private heroService: HeroService
   ) { 
@@ -25,19 +20,11 @@ export class HeroesComponent implements OnInit {
   }
   
   getHeroes(): void {
-    // this.heroes = this.heroService.getHeroes();
     this.heroService.getHeroes().subscribe(heroes => {
-      // console.log(typeof heroes);
-      // heroes.forEach(hero => {
-      //   console.log(hero);
-      // })
       this.heroes = heroes;
     })
   }
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
   ngOnInit() {
     console.log('init ngOninit');
     this.getHeroes();
